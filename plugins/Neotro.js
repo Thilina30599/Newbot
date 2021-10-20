418faf705448c1e44e456d9f6253544a07ef64e4
Thilina30599 <79817669+Thilina30599@users.noreply.github.com>
2021-10-20 16:29:00 +0530

    Update Neotro.js

--- plugins/Neotro.js
+++ plugins/Neotro.js
@@ -15,26 +15,6 @@ const Language = require('../language');
 const Lang = Language.getString('admin');
 const mut = Language.getString('mute');
 
-Alexa.addCommand({pattern: 'pubug ?(.*)', fromMe: true, desc: UUU,deleteCommand: true}, (async (message, match) => {    
-        if (match[1] == '') {
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.sendMessage(message.jid,DDO,MessageType.text);
-            await message.client.sendMessage(message.jid,DDO,MessageType.text);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.sendMessage(message.jid,ONO,MessageType.text);
-            await message.client.sendMessage(message.jid,".tagadmin",MessageType.text);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-        }
-        else {
-            return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
-        }
-
-}));
-
-
 Alexa.addCommand({pattern: 'awsh ?(.*)', fromMe: true, desc: UUU, dontAddCommandList: true}, (async (message, match) => {    
         if (match[1] == '') {
             await message.client.toggleDisappearingMessages(message.jid, 0);
